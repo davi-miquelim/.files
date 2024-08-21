6dc76b42c3048efc7d10c5aec8bbb58d178955d7
davi-miquelim <davichelimiquelim@gmail.com>
2024-08-21 17:12:58 -0300

    feat: golang tools

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -39,7 +39,6 @@ require("lazy").setup({
         {'hrsh7th/cmp-nvim-lsp'},
         {'L3MON4D3/LuaSnip'},
       },
-      opts = { buffer = bufnr, remap = false },
       config = function ()
           local cmp = require("cmp")
 
@@ -60,17 +59,17 @@ require("lazy").setup({
             }),
           })
 
-          vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-          vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end, opts)
-          vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-          vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, opts)
-          vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-          vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-          vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-          vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-          vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+          vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+          vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end)
+          vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+          vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end)
+          vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end)
+          vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
+          vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
+          vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end)
+          vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
       end
-    }, 
+    },
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
