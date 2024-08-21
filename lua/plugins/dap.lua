6d7b1d7bd9b75faa82d6b086558c06bb81734030
davi-miquelim <davichelimiquelim@gmail.com>
2024-08-21 16:56:34 -0300

    dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,7 +1,7 @@
 return {
     {
         'mfussenegger/nvim-dap',
-        config = function() 
+        config = function()
             local dap = require("dap")
             vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
             vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
